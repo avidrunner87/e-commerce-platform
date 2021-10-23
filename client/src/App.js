8f77a2f2d3d116ba4e3910c8a52da6c76007d909
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-10-23 10:56:56 -0500

    Added prettier and updated the app to make it work

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
@@ -19,45 +19,43 @@ import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+    uri: '/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    };
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <div>
-          <StoreProvider>
-            <Nav />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/success" element={<Success />} />
-              <Route path="/orderHistory" element={<OrderHistory />} />
-              <Route path="/products/:id"element={<Detail />} />
-              <Route element={<NoMatch />} />
-            </Routes>
-          </StoreProvider>
-        </div>
-      </BrowserRouter>
-    </ApolloProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <div>
+                <StoreProvider>
+                    <Nav />
+                    <Switch>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/success" element={<Success />} />
+                        <Route path="/orderHistory" element={<OrderHistory />} />
+                        <Route path="/products/:id" element={<Detail />} />
+                        <Route element={<NoMatch />} />
+                    </Switch>
+                </StoreProvider>
+            </div>
+        </ApolloProvider>
+    );
 }
 
 export default App;

1d7dfe3b09b525a807ff7e6d58fb72ba88bd7e61
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-10-23 20:20:20 -0500

    Redux added along with additional pictures

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { Provider } from 'react-redux';
+import store from './utils/store';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -14,50 +16,63 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+    uri: '/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : ''
+        }
+    };
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <div>
-          <StoreProvider>
-            <Nav />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/success" element={<Success />} />
-              <Route path="/orderHistory" element={<OrderHistory />} />
-              <Route path="/products/:id"element={<Detail />} />
-              <Route element={<NoMatch />} />
-            </Routes>
-          </StoreProvider>
-        </div>
-      </BrowserRouter>
-    </ApolloProvider>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <div>
+                    <Provider store={store}>
+                        <Nav />
+                        <Switch>
+                            <Route exact path="/">
+                                <Home />
+                            </Route>
+                            <Route exact path="/login">
+                                <Login />
+                            </Route>
+                            <Route exact path="/signup">
+                                <Signup />
+                            </Route>
+                            <Route exact path="/success">
+                                <Success />
+                            </Route>
+                            <Route exact path="/orderHistory">
+                                <OrderHistory />
+                            </Route>
+                            <Route path="/products/:id">
+                                <Detail />
+                            </Route>
+                            <Route>
+                                <NoMatch />
+                            </Route>
+                        </Switch>
+                    </Provider>
+                </div>
+            </BrowserRouter>
+        </ApolloProvider>
+    );
 }
 
 export default App;
